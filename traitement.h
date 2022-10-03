ff4e32376f6338e97b4ad1e1b552eae18e68a559
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-10-03 18:43:37 +0200

    Update traitement.h

--- traitement.h
+++ traitement.h
@@ -2,9 +2,9 @@
 #define TRAITEMENT_H
 
 SDL_Surface* load_image(const char* path);
-void print_pic(char image[]);
+void print_pic(SDL_Renderer* renderer,SDL_Texture* texture );
 Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format);
-void surface_to_grayscale(SDL_Surface* surface)
-Uint32 black_and_white(Uint32 pixel_color, SDL_PixelFormat* format);
-void surface_to_white(SDL_Surface* surface);
+void surface_to_grayscale(SDL_Surface* surface);
+
+
 #endif
