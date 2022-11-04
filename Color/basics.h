ff40aec10f05880d6d9ca5ae4820f60594ab80a8
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-04 19:34:15 +0100

    Update basics.h

--- Color/basics.h
+++ Color/basics.h
@@ -20,8 +20,10 @@ struct Image
 };
 typedef struct Image Image;
 
+
 SDL_Surface *load_image(const char *path);
 void save_image(SDL_Surface *image, char *path);
+static inline Uint8 *pixel_ref(SDL_Surface *surf, unsigned x, unsigned y);
 Uint32 get_pixel(SDL_Surface *surface, int x, int y);
 void put_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
 Image SDL_Surface_to_Image(SDL_Surface *surface);
