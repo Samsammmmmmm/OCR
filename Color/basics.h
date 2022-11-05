b425246d2c9b391cc5da561ac7bd80fc973c7a9e
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-05 13:24:23 +0100

    Update basics.h

--- Color/basics.h
+++ Color/basics.h
@@ -1,9 +1,7 @@
 #ifndef BASICS_H
 #define BASICS_H
 
-#include <gtk/gtk.h>
 #include <stdio.h>
-#include <maths.h>
 #include <err.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
@@ -24,21 +22,15 @@ struct Image
 };
 typedef struct Image Image;
 
-
+double clamp(double d, double min, double max);
 SDL_Surface *load_image(const char *path);
 void save_image(SDL_Surface *image, char *path);
-static inline Uint8 *pixel_ref(SDL_Surface *surf, unsigned x, unsigned y);
+Uint8 *pixel_ref(SDL_Surface *surf, unsigned x, unsigned y);
 Uint32 get_pixel(SDL_Surface *surface, int x, int y);
 void put_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
-Image SDL_Surface_to_Image(SDL_Surface *surface);
-SDL_Surface Image_to_SDL_Surface(Image image);
-int *Image_to_Array(Image *source);
-Image *Array_to_Image(int *source, int w, int h);
-void free_image(Image *image);
 int *image_grayscale_histogram(Image *image, int startx, int endx, int starty,
                                int endy);
 int get_histogram_min(int *hist);
 int get_histogram_max(int *hist);
-void image_filter(Image *image, int startx, int endx, int starty, int endy, int
-                  (*filter)(int));
+void print_pic(SDL_Renderer* renderer,SDL_Texture* texture );
 #endif
