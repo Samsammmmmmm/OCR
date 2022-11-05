cd6f8bb0e7cd76b81128202d3ef27be35e970245
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-05 13:25:59 +0100

    Update filter_funct.h

--- Color/filter_funct.h
+++ Color/filter_funct.h
@@ -1,17 +1,11 @@
-#ifndef FILTER_FUNCT.H
-#define FILTER_FUNCT.H
+#ifndef FILTER_FUNCT_H
+#define FILTER_FUNCT_H
 
 #include <err.h>
 #include <stdio.h>
 #include "basics.h"
 
-static void pixel_to_grayscale(Pixel *pixel, int brightness);
-static double gamma_filter(double p, int gamma);
-static double contrast_filter(double p, int contrast);
-static void pixel_to_gamma(Pixel *pixel, int gamma);
-static void pixel_to_contrast(Pixel *pixel, int contrast);
-void filter_grayscale(Image *image, double brigthness);
-void filter gamma_filter(Image *image, double gamma);
-void filter_contrast(Image *image, double contrast);
+Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format);
+void surface_to_grayscale(SDL_Surface* surface);
 
 #endif
