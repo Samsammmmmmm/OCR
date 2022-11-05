54847b98f2004c678fb7331bfeb2ee2ffcbcefce
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-05 13:24:59 +0100

    Update filter_funct.c

--- Color/filter_funct.c
+++ Color/filter_funct.c
@@ -1,61 +1,36 @@
 #include <stdio.h>
-#include <maths.h>
 #include <err.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
 #include <pthread.h>
 #include "basics.h"
 
-
-static void pixel_to_grayscale(Pixel *pixel, int brightness)
+// Converts a colored pixel into grayscale tyhen in black and white.
+//
+// pixel_color: Color of the pixel to convert in the RGB format.
+// format: Format of the pixel used by the surface.
+Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format)
 {
-    pixel->r = pixel->g = pixel->b = clamp(
-        (0.3 * pixel->r + 0.59 * pixel->g + 0.11 * pixel->b) + brightness, 0,
-        255);
+    Uint8 r, g, b;
+    SDL_GetRGB(pixel_color, format, &r, &g, &b);
+    Uint8 average = 0.3 * r + 0.59 * g + 0.11 * b;
+    Uint32 color = SDL_MapRGB(format, average, average, average);
+    return color;
 }
 
-static double gamma_filter(double p, int gamma)
+void surface_to_grayscale(SDL_Surface* surface)
 {
-    double c = gamma / 128;
-    double factor = pow(p / 255.0, c);
-    return clamp(factor * 255.0, 0, 255);
+    Uint32* pixels = surface->pixels;
+    int len = surface->w * surface->h;
+    SDL_PixelFormat* format = surface->format;
+    if (SDL_LockSurface(surface) != 0)
+        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    else
+    {
+        for (int i = 0; i < len; i++)
+        {
+            pixels[i] = pixel_to_grayscale(pixels[i], format);
+        }
+        SDL_UnlockSurface(surface);
+    }
 }
-
-static double contrast_filter(double p, int contrast)
-{
-    double c = contrast / 128;
-    double factor = (259 * (c + 255)) / (255 * (259 - c));
-    return clamp(factor * (p - 128) + 128, 0, 255);
-}
-
-
-static void pixel_to_gamma(Pixel *pixel, int gamma)
-{
-    pixel->r = gamma_filter(pixel->r, gamma);
-    pixel->g = gamma_filter(pixel->g, gamma);
-    pixel->b = gamma_filter(pixel->b, gamma);
-}
-
-static void pixel_to_contrast(Pixel *pixel, int contrast)
-{
-    pixel->r = contrast_filter(pixel->r, contrast);
-    pixel->g = contrast_filter(pixel->g, contrast);
-    pixel->b = contrast_filter(pixel->b, contrast);
-}
-
-void filter_grayscale(Image *image, double brigthness)
-{
-    image_filter(surface, pixel_to_grayscale, brigthness);
-}
-
-void filter gamma_filter(Image *image, double gamma)
-{
-    image_filter(surface, pixel_to_gamma, gamma);
-}
-
-void filter_contrast(Image *image, double contrast)
-{
-    image_filter(surface, pixel_to_contrast, contrast);
-}
-
-
