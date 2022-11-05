f58dfff05a5c7efa36e24cdcc90b145524f9fa65
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-05 13:26:34 +0100

    Update main.c

--- Color/main.c
+++ Color/main.c
@@ -2,61 +2,62 @@
 #include <err.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_image.h>
-#include "traitement.h"
+#include "basics.h"
+#include "filter_funct.h"
 
 int main(int argc, char** argv)
 {
-    // Checks the number of arguments.
+     // Checks the number of arguments.
     if (argc != 2)
         errx(EXIT_FAILURE, "Usage: image-file");
-    //printf("1\n");
 
     // Initializes the SDL.
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
-    //printf("2\n");
 
     // Creates a window.
-    SDL_Window* window = SDL_CreateWindow("Dynamic Fractal Canopy", 0, 0, 640,
-                                          400,SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    SDL_Window* window = SDL_CreateWindow("Dynamic Fractal Canopy", 0, 0, 640, 400,
+        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
     if (window == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
-    //printf("3\n");
 
     // Creates a renderer.
     SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
     if (renderer == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
-    //printf("4\n");
+
     // - Create a surface from the colored image.
     SDL_Surface* surface = load_image(argv[1]);
     if (surface == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
-    //printf("5\n");
-    save_image(surface, "ref.png");
-    SDL_FreeSurface(surface);
+    
     // Create a texture from the colored surface.
     SDL_Texture* texture = IMG_LoadTexture(renderer, argv[1]);
     if (texture == NULL)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
-    //printf("6\n");
-    // - Convert the surface into grayscale.
-    Image *img= surface_to_image(surface);
-    filter_grayscale(&img);
-    Surface result= image_to_surface(&img);
-    // - Free the surface.
-    save_image(result, "grayscale.png");
-    SDL_FreeSurface(result);
 
-    //printf("10\n");
+    // - Resize the window according to the size of the image.
+    SDL_SetWindowSize(window, surface->w, surface->h);
+
+    // - Convert the surface into grayscale.
+    surface_to_grayscale(surface);
+    save_image(surface, "ref.png");
+
+    // - Create a new texture from the grayscale surface.
+    SDL_Texture* result=SDL_CreateTextureFromSurface(renderer, surface);
+    
+    // - Free the surface.
+    SDL_FreeSurface(surface);
+    
     // - Dispatch the events.
     print_pic(renderer, result);
-    //printf("11\n");
+
     // - Destroy the objects.
     SDL_DestroyTexture(texture);
     SDL_DestroyTexture(result);
     SDL_DestroyRenderer(renderer);
+    SDL_DestroyWindow(window);
     SDL_Quit();
-    //printf("12\n");
+
     return EXIT_SUCCESS;
 }
