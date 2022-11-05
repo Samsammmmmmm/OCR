c3193e154464417d95732cf6d8e5a03ef054ffb8
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-05 15:38:17 +0100

    Update basics.h

--- Color/basics.h
+++ Color/basics.h
@@ -7,13 +7,11 @@
 #include <SDL2/SDL_image.h>
 
 
-double clamp(double d, double min, double max);
 SDL_Surface *load_image(const char *path);
 void save_image(SDL_Surface *image, char *path);
 Uint8 *pixel_ref(SDL_Surface *surf, unsigned x, unsigned y);
 Uint32 get_pixel(SDL_Surface *surface, int x, int y);
 void put_pixel(SDL_Surface *surface, int x, int y, Uint32 pixel);
-int *surface_to_histogram(SDL_Surface *surface) int get_histogram_min(int *hist);
-int get_histogram_max(int *hist);
+int *surface_to_histogram(SDL_Surface *surface);
 void print_pic(SDL_Renderer* renderer,SDL_Texture* texture );
 #endif
