fd9d214c811cac04339530749999ee577e1efe19
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-24 16:54:25 +0100

    Update filter_funct.h

--- Color/filter_funct.h
+++ Color/filter_funct.h
@@ -9,7 +9,7 @@ Uint32 pixel_to_grayscale(Uint32 pixel_color, SDL_PixelFormat* format);
 void surface_to_grayscale(SDL_Surface* surface);
 void otsu_tresholding(SDL_Surface *surface);
 void sauvola_tresholding(SDL_Surface *surface, int radius, float k);
-void median_filter(SDL_Surface *surface, int radius);
+void median_filter(SDL_Surface *surface);
 void gaussian_smoothing(SDL_Surface *surface, int sigma, int kernel);
 void contrast(SDL_Surface *surface);
 void gamma_filter(SDL_Surface *surface);
