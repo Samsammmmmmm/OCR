555cbf5114a854fa2b8599847eb8558cd9d5c09c
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-11-30 15:52:57 +0100

    Update main.c

--- Image_processing/main.c
+++ Image_processing/main.c
@@ -16,15 +16,15 @@ int main(int argc, char** argv)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
 
     // Creates a window.
-    SDL_Window* window = SDL_CreateWindow("Dynamic Fractal Canopy", 0, 0, 640, 400,
-        SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
-    if (window == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    //SDL_Window* window = SDL_CreateWindow("Dynamic Fractal Canopy", 0, 0, 640, 400,
+    //    SDL_WINDOW_SHOWN | SDL_WINDOW_RESIZABLE);
+    //if (window == NULL)
+    //    errx(EXIT_FAILURE, "%s", SDL_GetError());
 
     // Creates a renderer.
-    SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (renderer == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    //SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
+    //if (renderer == NULL)
+    //    errx(EXIT_FAILURE, "%s", SDL_GetError());
 
     // - Create a surface from the colored image.
     SDL_Surface* surface = load_image(argv[1]);
@@ -32,43 +32,44 @@ int main(int argc, char** argv)
         errx(EXIT_FAILURE, "%s", SDL_GetError());
     
     // Create a texture from the colored surface.
-    SDL_Texture* texture = IMG_LoadTexture(renderer, argv[1]);
-    if (texture == NULL)
-        errx(EXIT_FAILURE, "%s", SDL_GetError());
+    //SDL_Texture* texture = IMG_LoadTexture(renderer, argv[1]);
+    //if (texture == NULL)
+    //    errx(EXIT_FAILURE, "%s", SDL_GetError());
 
     // - Resize the window according to the size of the image.
-    SDL_SetWindowSize(window, surface->w, surface->h);
+    //SDL_SetWindowSize(window, surface->w, surface->h);
 
-    //copy surface existing
-    SDL_Surface* detect_ligns = *surface;
-    // - Do the processing.
+    // - Create a surface from the colored image.
+    SDL_Surface* detect_ligns = surface;
     
+    // - Do the processing.
     gamma_filter(surface);
     contrast(surface);
     otsu_tresholding(surface);
     median_filter(surface);
     save_image(surface, "result.png");
-
-    gamma_filter(detec_ligns);
-    contrast(detec_ligns);
-    sobel_filter(detec_ligns);
-    otsu_tresholding(detec_ligns);
-    save_image(detec_ligns, "ligns.png");
+    
+    gamma_filter(detect_ligns);
+    contrast(detect_ligns);
+    sobel_filter(detect_ligns);
+    otsu_tresholding(surface);
+    save_image(detect_ligns, "ligns.png");
 
     // - Create a new texture from the grayscale surface.
-    SDL_Texture* result=SDL_CreateTextureFromSurface(renderer, surface);
-    
+    //SDL_Texture* result=SDL_CreateTextureFromSurface(renderer, surface);
+
+    // - Dispatch the events.
+    //print_pic(renderer, result);
+
     // - Free the surface.
     SDL_FreeSurface(surface);
+    SDL_FreeSurface(detect_ligns);
     
-    // - Dispatch the events.
-    print_pic(renderer, result);
-
     // - Destroy the objects.
-    SDL_DestroyTexture(texture);
-    SDL_DestroyTexture(result);
-    SDL_DestroyRenderer(renderer);
-    SDL_DestroyWindow(window);
+    //SDL_DestroyTexture(texture);
+    //SDL_DestroyTexture(result);
+    //SDL_DestroyRenderer(renderer);
+    //SDL_DestroyWindow(window);
     SDL_Quit();
 
     return EXIT_SUCCESS;
