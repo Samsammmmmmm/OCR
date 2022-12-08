29cbe37460a84a77652e1634e45c5fe41501b5da
Gwennj <96210472+Gwennj@users.noreply.github.com>
2022-12-08 17:16:25 +0100

    Create rota.h

---
+++ Image_processing/Rotation/rota.h
@@ -0,0 +1,20 @@
+#include <err.h>
+/*#include </opt/homebrew/include/SDL2/SDL.h>
+  #include </opt/homebrew/Cellar/sdl2_image/2.6.2/include/SDL2/SDL_image.h>*/
+#include "../Tools/basics.h"
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <math.h>
+
+void draw(SDL_Renderer* renderer, SDL_Texture* texture, double angle);
+
+void event_loop(SDL_Renderer* renderer, SDL_Texture* texture, double angle_p);
+
+SDL_Surface* load_image(const char* path);
+
+SDL_Surface* manual_rota(SDL_Surface * surface, int degree);
+
+int main(int argc, char** argv);
