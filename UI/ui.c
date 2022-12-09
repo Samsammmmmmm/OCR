7d6c8adf7be67773d149fec783bf7e61e0687148
lisa.bourlier <lisa.bourlier@epita.fr>
2022-12-09 13:10:59 +0100

    charging the image with bugs

--- UI/ui.c
+++ UI/ui.c
@@ -7,15 +7,26 @@
 
 // Makes the widgets global
 GtkBuilder* builder;
-GtkWidget* window;
-GtkWidget* fixed;
-GtkWidget* button_parameters;
-GtkWidget* button_start;
-GtkWidget* button_save;
-GtkWidget* label_step;
-GtkWidget* button_file_chooser;
-GtkWidget* grid;
+GtkWindow* window;
+GtkFixed* fixed;
+GtkButton* button_parameters;
+GtkButton* button_start;
+GtkButton* button_save;
+GtkLabel* label_step;
+GtkFileChooserButton* button_file_chooser;
+GtkImage* grid;
 gchar *file_name = NULL;
+
+// function that recreate the new image selected
+void change_image(GtkContainer* fixed, gchar* file_name, GtkWidget* grid)
+{
+    int hor = 270, ver = 100; // place the image manually **to modify**
+    gtk_container_remove(fixed, grid); //remove the old grid
+    grid = gtk_image_new_from_file (file_name);
+    gtk_widget_show(grid);
+    gtk_container_add(fixed, grid);
+    gtk_fixed_move((GtkFixed*) fixed, grid, hor, ver);
+}
 
 //event handler for the "on_file_set" signal. Allows to choose the starting grid
 void on_file_set(GtkFileChooserButton *f)
@@ -24,12 +35,7 @@ void on_file_set(GtkFileChooserButton *f)
     printf("file name: %s\n", file_name);
     printf("folder name: %s\n", gtk_file_chooser_get_current_folder (GTK_FILE_CHOOSER(f)));
     //charge the image
-    /*int hor = 150, ver = 1;
-    gtk_container_remove(GTK_CONTAINER (fixed), grid); //remove the old grid
-    grid = gtk_image_new_from_file (file_name);
-    gtk_widget_show(grid);
-    gtk_container_add(GTK_CONTAINER (fixed), grid);
-    gtk_fixed_move(GTK_FIXED(fixed), grid, hor, ver);*/
+    change_image((GtkContainer*) fixed, file_name, (GtkWidget*) grid);
     //change the step label to charging the picture
     gtk_label_set_label((GtkLabel*)label_step, "step in progress : charging the image\n");
 }
@@ -54,28 +60,28 @@ int main (int argc, char *argv[])
     }
 
     // Gets the widgets.
-    window = GTK_WIDGET(gtk_builder_get_object(builder, "window"));
-    fixed = GTK_WIDGET(gtk_builder_get_object(builder, "fixed"));
-    button_parameters = GTK_WIDGET(gtk_builder_get_object(builder, "button_parameters"));
-    button_start = GTK_WIDGET(gtk_builder_get_object(builder, "button_start"));
-    button_save = GTK_WIDGET(gtk_builder_get_object(builder, "button_save"));
-    label_step = GTK_WIDGET(gtk_builder_get_object(builder, "label_step"));
-    button_file_chooser = GTK_WIDGET(gtk_builder_get_object(builder, "button_file_chooser"));
-    grid = GTK_WIDGET(gtk_builder_get_object(builder, "grid"));
+    window = GTK_WINDOW(gtk_builder_get_object(builder, "window"));
+    fixed = GTK_FIXED(gtk_builder_get_object(builder, "fixed"));
+    button_parameters = GTK_BUTTON(gtk_builder_get_object(builder, "button_parameters"));
+    button_start = GTK_BUTTON(gtk_builder_get_object(builder, "button_start"));
+    button_save = GTK_BUTTON(gtk_builder_get_object(builder, "button_save"));
+    label_step = GTK_LABEL(gtk_builder_get_object(builder, "label_step"));
+    button_file_chooser = GTK_FILE_CHOOSER_BUTTON(gtk_builder_get_object(builder, "button_file_chooser"));
+    grid = GTK_IMAGE(gtk_builder_get_object(builder, "grid"));
     //grid = NULL;
 
     // Connects event handlers.
     g_signal_connect(window, "destroy", G_CALLBACK(gtk_main_quit), NULL);
-    /* g_signal_connect(button_parameters, "clicked", G_CALLBACK(on_parameters), &game);
-    g_signal_connect(stop_button, "clicked", G_CALLBACK(on_stop), &game);
-    g_signal_connect(window, "key_press_event", G_CALLBACK(on_key_press), &game);
-    g_signal_connect(window, "key_release_event", G_CALLBACK(on_key_release), &game);
-    g_signal_connect(area, "configure-event", G_CALLBACK(on_configure), &game);
-    g_signal_connect(training_cb, "toggled", G_CALLBACK(on_training), &game);*/
+    //g_signal_connect(button_parameters, "clicked", G_CALLBACK(on_parameters), button_parameters);
+    //g_signal_connect(stop_button, "clicked", G_CALLBACK(on_stop), &game);
+    g_signal_connect(button_file_chooser, "file-set", G_CALLBACK(on_file_set), NULL);
+    // g_signal_connect(window, "key_release_event", G_CALLBACK(on_key_release), &game);
+    //g_signal_connect(area, "configure-event", G_CALLBACK(on_configure), &game);
+    //g_signal_connect(training_cb, "toggled", G_CALLBACK(on_training), &game);
 
      // Runs the main loop
     gtk_builder_connect_signals(builder, NULL);
-    gtk_widget_show(window);// display the window
+    gtk_widget_show((GtkWidget*) window);// display the window
     gtk_main();
 
     // Exits.
